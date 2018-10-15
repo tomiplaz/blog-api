a01aab3a30a694c729096cee817e2316d2c48a5c
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-15 17:57:46 +0200

    Fix fetching posts by tag.

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
-use App\Tag as TagModel;
+use App\Post as PostModel;
 
 class TagController extends BaseController
 {
@@ -12,9 +12,9 @@ class TagController extends BaseController
      *
      * @return void
      */
-    public function __construct(TagModel $tagModel)
+    public function __construct(PostModel $postModel)
     {
-        $this->tagModel = $tagModel;
+        $this->postModel = $postModel;
     }
 
     /**
@@ -25,6 +25,11 @@ class TagController extends BaseController
      * @return \Illuminate\Database\Eloquent\Collection
      */
     public function tagPosts(string $key) {
-        return $this->tagModel->where('key', $key)->posts();
+        return $this->postModel
+            ->with(['user', 'comments', 'tags'])
+            ->whereHas('tags', function ($query) use ($key) {
+                $query->where('key', $key);
+            })
+            ->get();
     }
 }
