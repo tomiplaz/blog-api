96daf1d8cfe856092068d0fbdb275fffed34e288
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-10-27 16:08:19 +0200

    Add about column to users table.

--- database/migrations/2017_07_31_194006_create_users_table.php
+++ database/migrations/2017_07_31_194006_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('name', 20)->unique();
             $table->string('email', 100)->unique();
             $table->string('password', 255);
+            $table->string('about', 255)->nullable();
             $table->timestamps();
         });
     }
