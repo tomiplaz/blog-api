fade10037df312e64beb7f06c1aab5996c5c2609
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-11-21 19:24:52 +0100

    Add index view.

---
+++ resources/views/index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Blogging App</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="blog blogs blogging app free simple">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <app-root></app-root>
+  <script type="text/javascript" src="inline.bundle.js"></script>
+  <script type="text/javascript" src="polyfills.bundle.js"></script>
+  <script type="text/javascript" src="styles.bundle.js"></script>
+  <script type="text/javascript" src="vendor.bundle.js"></script>
+  <script type="text/javascript" src="main.bundle.js"></script>
+</body>
+</html>
