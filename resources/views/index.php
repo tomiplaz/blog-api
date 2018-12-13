c06f53a8dfbc708f60e263444109f0df8af6e6c0
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-13 21:11:31 +0100

    Update client app files: styling fixes.

--- resources/views/index.php
+++ resources/views/index.php
@@ -6,8 +6,8 @@
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles.6f374364e7f357356e08.css"></head>
+<link rel="stylesheet" href="styles.3a84f4634bc41e0d993c.css"></head>
 <body>
   <app-root></app-root>
-<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script><script type="text/javascript" src="polyfills.f21ca58529fdfb26673c.js"></script><script type="text/javascript" src="main.07739d02569f492332cc.js"></script></body>
+<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script><script type="text/javascript" src="polyfills.f21ca58529fdfb26673c.js"></script><script type="text/javascript" src="main.4739a72c7df04b9d44b0.js"></script></body>
 </html>
