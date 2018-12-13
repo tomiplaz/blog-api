ffd3bc22ad486331890c78806c188ddaa93647f7
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-13 18:06:34 +0100

    Update client app files after migration to latest Angular.

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,1 +1,13 @@
-<!doctype html><html lang="en"><head><meta charset="utf-8"><title>blogs: a blogging app</title><base href="/"><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="icon" type="image/x-icon" href="favicon.ico"><link href="styles.85f2fa0f4e52864f1475.bundle.css" rel="stylesheet"/></head><body><app-root></app-root><script type="text/javascript" src="inline.318b50c57b4eba3d437b.bundle.js"></script><script type="text/javascript" src="polyfills.50b851ccd19aa9b94ce1.bundle.js"></script><script type="text/javascript" src="main.763b71f4429ca479b9cc.bundle.js"></script></body></html>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>blogs: a blogging app</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+<link rel="stylesheet" href="styles.6f374364e7f357356e08.css"></head>
+<body>
+  <app-root></app-root>
+<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script><script type="text/javascript" src="polyfills.f21ca58529fdfb26673c.js"></script><script type="text/javascript" src="main.07739d02569f492332cc.js"></script></body>
+</html>
