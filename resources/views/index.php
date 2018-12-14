82cd9fb2041018d5c77a025541d0d425e3114793
Tomislav Plazonic <tomislav.plazonic@gmail.com>
2018-12-14 17:58:52 +0100

    Update client app files.

--- resources/views/index.php
+++ resources/views/index.php
@@ -1,13 +1,23 @@
 <!doctype html>
 <html lang="en">
 <head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-115146709-2"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-115146709-2');
+  </script>
   <meta charset="utf-8">
   <title>blogs: a blogging app</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="keywords" content="blogs blogging app blog post comment posts comments">
+  <meta name="author" content="Tomislav Plazonic">
+  <meta name="description" content="Create your own blog, read and comment on other peoples' blogs.">
   <link rel="icon" type="image/x-icon" href="favicon.ico">
-<link rel="stylesheet" href="styles.3a84f4634bc41e0d993c.css"></head>
+<link rel="stylesheet" href="styles.b5e7533d7bc95593e8a3.css"></head>
 <body>
   <app-root></app-root>
-<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script><script type="text/javascript" src="polyfills.f21ca58529fdfb26673c.js"></script><script type="text/javascript" src="main.4739a72c7df04b9d44b0.js"></script></body>
+<script type="text/javascript" src="runtime.ec2944dd8b20ec099bf3.js"></script><script type="text/javascript" src="polyfills.f21ca58529fdfb26673c.js"></script><script type="text/javascript" src="main.e3969cf0a0437e998ef5.js"></script></body>
 </html>
